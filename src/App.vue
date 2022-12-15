84455a02cbe20318b015abd6ceb591b6e34f8b97
Brian Grant <brian@skeletonjack.net>
2022-12-14 20:57:12 -0600

    Managing complex child components: end

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,35 @@
 <template>
-  <div id="app" class="container mt-5">
-    <h1>My Shop</h1>
-    <p class="animated fadeInRight">Take a look at our offerings</p>
-    <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
-		<price value="4.52"></price>
-  </div>
+	<div id="app" class="container mt-5">
+		<h1>My Shop</h1>
+		<p class="animated fadeInRight">Take a look at our offerings</p>
+		<font-awesome-icon icon="shopping-cart"></font-awesome-icon>
+		<ProductList :maximum="maximum" :products="products"></ProductList>
+	</div>
 </template>
 
 <script>
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-import Price from './components/Price.vue' // import Price from '@/components/Price.vue';
+import ProductList from '@/components/ProductList.vue'; // import Price from '@/components/Price.vue';
 
 export default {
-  name: "app",
-  components: {
-    FontAwesomeIcon,
-		Price
-  }
+	name: "app",
+	data: function () {
+		return {
+			maximum: 99,
+			products: null
+		}
+	},
+	components: {
+		ProductList,
+		FontAwesomeIcon,
+	},
+	mounted: function () {
+		fetch('https://hplussport.com/api/products/order/price')
+				.then(response => response.json())
+				.then(data => {
+					this.products = data;
+				})
+	}
 };
 </script>
