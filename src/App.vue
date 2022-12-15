9fa1473563f0175a27afe50795b5dcb1f54aabea
Brian Grant <brian@skeletonjack.net>
2022-12-14 21:57:07 -0600

    Using the Chrome DevTools

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 		<h1>My Shop</h1>
 		<p class="animated fadeInRight">Take a look at our offerings</p>
 		<font-awesome-icon icon="shopping-cart"></font-awesome-icon>
-		<ProductList :maximum="maximum" :products="products"></ProductList>
+		<ProductList :maximum="maximum" :products="products" @add="addItem"></ProductList>
 	</div>
 </template>
 
@@ -17,12 +17,40 @@ export default {
 	data: function () {
 		return {
 			maximum: 99,
+			cart: [],
 			products: null
 		}
 	},
 	components: {
 		ProductList,
 		FontAwesomeIcon,
+	},
+	methods: {
+		addItem: function (product) {
+			console.log(product);
+			let whichProduct;
+			const existing = this.cart.filter(function (item, index) {
+				if (item.product.id == Number(product.id)) {
+					whichProduct = index;
+					return true;
+				} else {
+					return false;
+				}
+			});
+
+			if (existing.length) {
+				this.cart[whichProduct].qty++
+			} else {
+				this.cart.push({ product: product, qty: 1 })
+			}
+		},
+		deleteItem: function (id) {
+			if (this.cart[id].qty > 1) {
+				this.cart[id].qty--;
+			} else {
+				this.cart.splice(id, 1);
+			}
+		}
 	},
 	mounted: function () {
 		fetch('https://hplussport.com/api/products/order/price')
